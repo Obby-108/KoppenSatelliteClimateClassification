681b380114e2f30ff43ed1c73f4bbbb2633966af
Brian Salchert <brisalchert@gmail.com>
2026-04-15 12:00:55 -0400

    Update training hyperparameters and reduce transformer layers to prevent overfitting

--- model/swin_transformer.py
+++ model/swin_transformer.py
@@ -1,15 +1,26 @@
 import torch
 from torch import nn
-from torchvision.models import swin_t, Swin_T_Weights
+from torchvision.models import SwinTransformer
 
-class SwinTransformer(nn.Module):
+
+class SwinClimateTransformer(nn.Module):
     def __init__(self, num_classes=30, in_channels=12):
-        super(SwinTransformer, self).__init__()
-        self.model = swin_t(weights=Swin_T_Weights.DEFAULT)
+        super(SwinClimateTransformer, self).__init__()
+        self.model = SwinTransformer(
+            patch_size=[4, 4],
+            embed_dim=96,
+            depths=[2, 2, 2, 2], # Reduce layer 3 depth to decrease overfitting
+            num_heads=[3, 6, 12, 24],
+            window_size=[8, 8],
+            dropout=0.2,
+            stochastic_depth_prob=0.1,
+            num_classes=num_classes
+        )
 
-        # Modify the Patch Embedding (Input Layer) for 12 channels
+        # Modify the Patch Embedding for 12 channels
         old_patch_embed = self.model.features[0][0]
-        new_conv = nn.Conv2d(
+
+        self.model.features[0][0] = nn.Conv2d(
             in_channels=in_channels,
             out_channels=old_patch_embed.out_channels,
             kernel_size=old_patch_embed.kernel_size,
@@ -17,15 +28,11 @@ class SwinTransformer(nn.Module):
             padding=old_patch_embed.padding
         )
 
-        # Pretrained Weight Expansion (The "Warm Start")
+        # Pretrained Weight Expansion (Averaging + Repeating)
         with torch.no_grad():
-            # Average the 3 RGB weights [96, 3, 4, 4] -> [96, 1, 4, 4]
             avg_weights = old_patch_embed.weight.mean(dim=1, keepdim=True)
-            # Repeat to fill 12 channels [96, 12, 4, 4]
-            new_conv.weight.copy_(avg_weights.repeat(1, in_channels, 1, 1))
-            new_conv.bias.copy_(old_patch_embed.bias)
-
-        self.model.features[0][0] = new_conv
+            self.model.features[0][0].weight.copy_(avg_weights.repeat(1, in_channels, 1, 1))
+            self.model.features[0][0].bias.copy_(old_patch_embed.bias)
 
         # Modify the Head
         self.model.head = nn.Linear(
